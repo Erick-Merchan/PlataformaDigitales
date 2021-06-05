cae4b7f11b2cc5f71bceebd939bdf01e6d2ba05e
Erick Merchan <erickmerchanbravo@gmail.com>
2021-06-04 23:11:48 -0500

    first commit

---
+++ PYTHON/ejercicio002.py
@@ -0,0 +1,5 @@
+print('*****BIENVENIDO AL EJERCICIO 002*****')
+lado = int(input('porfavor ingrese el lado del cuadrado: \n' ))
+perimetro = lado * 4
+area = lado * lado 
+print(f'El perímetro es {perimetro} y el área es {area}')
---
+++ PYTHON/ejercicio003.py
@@ -0,0 +1,3 @@
+#Esto es una prueba de git
+#hola como estas
+print('hola como estas')
