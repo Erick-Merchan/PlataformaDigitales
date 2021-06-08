1e6071f48537b77142c2d3019d81041707b0caab
Erick Merchan <erickmerchanbravo@gmail.com>
2021-06-07 20:53:00 -0500

    ejercicio006

---
+++ PYTHON/Ejercicio006.py
@@ -0,0 +1,7 @@
+print('*****BIENVENIDO AL EJERCICIO 005*****')
+Altura=float(input('ingrese la altura del triangulo: '))
+Base=float(input('ingrese la base del triangulo: '))
+
+
+Area=((Altura*Base)/2)
+print(f'El Area del triangulo es {Area}')
---
+++ PYTHON/ejercicio005.py
@@ -0,0 +1,17 @@
+import math
+print('*****BIENVENIDO AL EJERCICIO 005*****')
+radio = int(input('Por favor ngresar el radio del circulo : \n'))
+
+A = math.pi * (radio*radio)
+P = 2 * math.pi * radio
+
+print(f'El area del circulo es : {A}')
+print(f'El perimetro del circulo es : {P}') 
+
+radio = int(input('Ingrese el radio del circulo : \n'))
+
+A = math.pi * (radio*radio)
+P = 2 * math.pi * radio
+
+print(f'El area del circulo es : {A}')
+print(f'El perimetro del circulo es : {P}')
